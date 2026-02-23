41f3420ca082a965c88b3e7172ab744507917a4f
Ademola Ajibade <ajibadeademola34@gmail.com>
2026-02-23 08:02:46 +0100

    login&logout implemented

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -8,6 +8,7 @@ export const useAuthStore = create((set) => ({
   authUser: null,
   isCheckingAuth: true,
   isSigningUp: false,
+  isLogging: false,
 
   checkAuth: async () => {
     try {
@@ -29,14 +30,37 @@ export const useAuthStore = create((set) => ({
       console.log(res.data);
       set({ authUser: res.data.user });
 
-
       //
       toast.success("Signup successful! Welcome to Chatty App.");
-
     } catch (error) {
       toast.error(error.response.data.message || "Failed to signup");
     } finally {
       set({ isSigningUp: false });
+    }
+  },
+  login: async (data) => {
+    set({ isLogging: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      console.log(res.data);
+      set({ authUser: res.data.user });
+
+      //
+      toast.success("Login successful! Welcome back to Chatty App.");
+    } catch (error) {
+      toast.error(error.response.data.message || "Failed to login");
+    } finally {
+      set({ isLogging: false });
+    }
+  },
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully!");
+    } catch (error) {
+      toast.error("Failed to logout. Please try again.");
+      console.log("Logout error:", error);
     }
   },
 }));
